c5c93de245863ba54d7dd2d25931245d8c6288cf
fabian <fabian@fabianism.us>
2014-07-24 15:32:53 -0400

    Initial commit

---
+++ scrapi-tools/scrapi_exceptions.py
@@ -0,0 +1,7 @@
+"""
+    A collection of exceptions for consumers.py
+"""
+
+
+class MissingAttributeError(Exception):
+    pass
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='scrAPI tools',
+    version='0.1.0',
+    author='Fabian von Feilitzsch',
+    author_email='fabian@fabianism.us',
+    packages=['scrapi-tools'],
+    url='http://pypi.python.org/pypi/scrAPI-tools/',
+    license='LICENSE.txt',
+    description='Package to aid in consumer creation for scrAPI',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
