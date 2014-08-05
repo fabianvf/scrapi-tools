bc93459675abec93352fca48c68ac3b704ed4c9b
fabian <fabian@fabianism.us>
2014-08-05 10:03:44 -0400

    Add additional typechecking for linter

--- scrapi_tools/manager.py
+++ scrapi_tools/manager.py
@@ -32,20 +32,36 @@ def lint(consume, normalize):
         Runs the consume and normalize functions, ensuring that
         they match the requirements of scrAPI.
     """
-    errors = []
+    errors = {}
     output = consume()
     if not isinstance(output, list):
-        errors.append("consume() does not return type list")
+        errors.add("consume() does not return type list")
         return errors
 
     normalized_output = []
     for doc in output:
         if not isinstance(doc, RawDocument):
-            errors.append("consume() does not return a list of type [RawDocument]")
+            errors.add("consume() does not return a list of type [RawDocument]")
         else:
             normalized_output.append(normalize(doc, datetime.datetime.now))
 
     for doc in normalized_output:
         if not isinstance(doc, NormalizedDocument):
-            errors.append("normalize does not return type NormalizedDocument")
+            errors.add("normalize does not return type NormalizedDocument")
+        else:
+            try:
+                title = doc.get("title")
+                contributors = doc.get("contributors")
+                id = doc.get("id")
+                source = doc.get("source")
+                if not isinstance(title, str):
+                    errors.add("Normalize does not return a string for the title")
+                if not isinstance(contributors, dict) or contributors.get("email") is None or contributors.get("full_name") is None:
+                    errors.add("Normalize does not return contributors as a dictionary of {email: {EMAIL}, full_name: {FULL_NAME}}")
+                if not isinstance(id, str):
+                    errors.add("Normalize does not return a string for the unique identifier")
+                if not isinstance(source, str):
+                    errors.add("Normalize does not return a string for the source")
+            except AttributeError:
+                errors.add("Was not able to retrieve information from the NormalizedFile using '.get()'")
     return errors
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='scrapi_tools',
-    version='0.2.0',
+    version='0.3.0',
     author='Fabian von Feilitzsch',
     author_email='fabian@fabianism.us',
     packages=['scrapi_tools'],
     url='http://www.github.com/fabianvf/scrapi-tools',
     license='LICENSE.txt',
     description='Package to aid in consumer creation for scrAPI',
-    long_description=open('README.md').read(),
+    long_description=open('README').read(),
     install_requires=[],
 )
